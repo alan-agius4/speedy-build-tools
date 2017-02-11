dd3034b95d08ef9d4a50e426c140a637fa629854
Alan Agius <alan.agius4@gmail.com>
2017-02-11 20:45:43 +0100

    test(utils): add spec for utils
    * test(utils): add spec for utils
    
    * style(utils spec): add breakline

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,10 +1,31 @@
-import { toArray } from "./utils";
+import * as glob from "glob";
+import { toArray, globArray } from "./utils";
 
 describe("utilsSpec", () => {
+
     describe("toArray", () => {
         it("must convert value to array", () => {
             const value = "hello world";
             expect(toArray(value)).toEqual([value]);
         });
     });
+
+    describe("globArray", () => {
+        const filesExludingSpec = ["test.ts"];
+        const specFiles = ["test.spec.ts"];
+        const allFiles = [...filesExludingSpec, ...specFiles];
+
+        beforeEach(() => {
+            spyOn(glob, "sync").and.returnValues(allFiles, specFiles);
+        });
+
+        it("must return all paths matching pattern", () => {
+            expect(globArray(["*.ts"])).toEqual(allFiles);
+        });
+
+        it("must return paths excluding negative patterns", () => {
+            expect(globArray(["*.ts", "!*.spec.ts"])).toEqual(filesExludingSpec);
+        });
+    });
+
 });
