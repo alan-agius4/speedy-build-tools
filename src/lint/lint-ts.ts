ec8a16501178560d9bec735c619fb1b0240498fd
Alan Agius <alan.agius4@gmail.com>
2017-02-27 09:23:29 +0100

    Feature/logger (#14)
    feat(worker client): pass args to forked process

--- src/lint/lint-ts.ts
+++ src/lint/lint-ts.ts
@@ -1,41 +1,91 @@
-import { Linter, LintResult } from "tslint";
+import { Linter, LintResult, Configuration } from "tslint";
+import { resolve } from "path";
+import { CommandModule, Argv, argv } from "yargs";
 
-import { Logger } from "../utils/logger";
-import { Timer } from "../utils/timer";
-import { Worker } from "../utils/worker/worker.client";
-import { readFileAsync, globArray, toArray } from "../utils/utils";
+import {
+	Logger,
+	Worker,
+	Timer,
+	readFileAsync,
+	globArray,
+	toArray,
+	Args,
+	Arguments
+} from "../utils";
 
 const logger = new Logger("Lint TS");
 const timer = new Timer(logger);
+let configData: Configuration.IConfigurationLoadResult;
 
-export function lintTs(pattern: string | string[]): Promise<{}> {
+interface LintTSArguments extends Arguments {
+	files: string[];
+	config: string;
+	continueOnError: boolean;
+}
+
+export function lintTs(files?: string | string[], config?: string): Promise<LintResult[]> {
 	timer.start();
 
-	return Worker.run(__filename, "lintTsWorker", pattern)
+	return Worker.run(__filename, lintTsWorker.name, files)
 		.then(x => {
 			timer.finish();
 			return x;
 		})
-		.catch(error => logger.error(error));
+		.catch(error => logger.error(null, error));
 }
 
-export function lintTsWorker(pattern: string | string[]): Promise<LintResult[]> {
-	const promises = globArray(toArray(pattern)).map(x => lintFile(x));
-	return Promise.all(promises);
+export function lintTsWorker(files?: string | string[], config?: string): Promise<LintResult[]> {
+	const args = setupArgs<LintTSArguments>();
+
+	if (!files) {
+		files = args.files;
+	}
+
+	if (!config) {
+		config = args.config;
+	}
+
+	configData = Configuration.findConfiguration(null, config).results!;
+	const promises = globArray(toArray(files)).map(x => lintFile(x));
+
+	return Promise
+		.all(promises)
+		.then(result => {
+			const failures = result.filter(x => x.failureCount > 0);
+			failures.forEach(x => logger.info(x.output));
+
+			if (!args.continueOnError) {
+				process.exit(1);
+			}
+
+			return failures;
+		});
 }
 
-export function lintFile(filePath: string): Promise<LintResult> {
+function lintFile(filePath: string): Promise<LintResult> {
 	return readFileAsync(filePath)
 		.then(fileContents => {
 			const linter = new Linter({
-				formatter: null!,
-				formattersDirectory: null!,
-				rulesDirectory: null!,
+				formatter: "stylish",
 				fix: false
 			});
 
-			linter.lint(filePath, fileContents, {});
+			linter.lint(filePath, fileContents, configData);
 
 			return linter.getResult();
 		});
 }
+
+function setupArgs<T>(yargs?: Argv): T {
+	Args.setBoolean("continueOnError", false);
+	Args.set("files", "./src/**/*.ts", "f");
+	Args.set("config", resolve(__dirname, "../tslint.json"), "c");
+	return yargs ? yargs.argv : argv;
+}
+
+export const tsLintModule: CommandModule = {
+	command: "lint-ts",
+	describe: "Lint typescript files",
+	builder: setupArgs,
+	handler: lintTs
+};
