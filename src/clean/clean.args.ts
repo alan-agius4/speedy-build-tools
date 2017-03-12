ece607643ffb39ca148ddc6a0d8d8a463fcef11c
Alan Agius <alan.agius4@gmail.com>
2017-03-12 15:10:38 +0100

    minor fixes in clean task

--- src/clean/clean.args.ts
+++ src/clean/clean.args.ts
@@ -6,6 +6,7 @@ export const ARGS: ArgumentOptions<CleanOptions>[] = [
 		key: "paths",
 		alias: "p",
 		description: "Paths to be deleted  - Supports glob patterns",
-		array: true
+		array: true,
+		required: true
 	}
 ];
