d1ef553285cd275f8ba2c3c561d4c5bd96f4d334
Alan Agius <alan.agius4@gmail.com>
2017-03-12 09:50:23 +0100

    chore(lint): add lint to ci

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,7 +12,7 @@ export class Logger {
 	}
 
 	info(message: string) {
-		console.log(white(this.formatMessage(message)));
+		console.info(white(this.formatMessage(message)));
 	}
 
 	debug(method: string, message: string) {
