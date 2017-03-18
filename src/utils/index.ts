00df34028d43628257277edfd728f7b56682c94b
Alan Agius <alan.agius4@gmail.com>
2017-03-18 15:19:55 +0100

    chore(utils): add `Dictionary` interface

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,9 @@
 export * from "./file-system";
-
 export * from "./worker";
 export * from "./args";
+export * from "./string";
+export * from "./dictionary";
 
 export { Logger } from "./logger";
 export { Timer } from "./timer";
-
 export { CommandModule, buildCommandModule } from "./command-module";
