47bcdafc4e65ad6df630974ea4fe540c35473285
Alan Agius <alan.agius@betssongroup.com>
2017-05-18 14:14:52 +0200

    perf(all): implement caching of resolved `require` paths

---
+++ src/utils/require-cache.ts
@@ -0,0 +1,11 @@
+import { RequireCache } from "@speedy/require-cache";
+import { packageMeta } from "@speedy/node-core";
+import { join } from "path";
+
+export function initCache(readOnlyMode = false) {
+	new RequireCache({
+		readOnlyMode,
+		cacheFilePath: join(__dirname, "../.cache/require-cache.json"),
+		cacheKiller: packageMeta.getVersion(join(__dirname, "../package.json"))
+	}).start();
+}
--- src/utils/worker/worker.process.ts
+++ src/utils/worker/worker.process.ts
@@ -1,11 +1,13 @@
 import { Logger } from "@speedy/node-core";
 
+import { initCache } from "../require-cache";
 import { WorkerMessage } from "./worker.model";
 
 const logger = new Logger("Worker Process");
 
 process.on("message", async (message: WorkerMessage) => {
 	try {
+		initCache(true);
 		const task = require(message.modulePath)[message.task] as (...params: any[]) => Promise<any>;
 		const result = await task.call(null, message.parameters) as Promise<any>;
 
