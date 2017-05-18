e6601d03e850a1b26e3ed0e738b29d334e18a813
Alan Agius <alan.agius@betssongroup.com>
2017-05-18 14:33:22 +0200

    fix(require cache): update relative path

--- src/utils/require-cache.ts
+++ src/utils/require-cache.ts
@@ -5,7 +5,7 @@ import { join } from "path";
 export function initCache(readOnlyMode = false) {
 	new RequireCache({
 		readOnlyMode,
-		cacheFilePath: join(__dirname, "../.cache/require-cache.json"),
-		cacheKiller: packageMeta.getVersion(join(__dirname, "../package.json"))
+		cacheFilePath: join(__dirname, "../../.cache/require-cache.json"),
+		cacheKiller: packageMeta.getVersion(join(__dirname, "../../package.json"))
 	}).start();
 }
